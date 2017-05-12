189761157b23816dee1284b1c1eef54061bfdeb5
Binh Duong <bduong@pushtechnology.com>
2017-05-12 22:40:00 +0100

    Tidy up Game hashCode() and equals()
    Add a unit test

--- src/main/java/org/adani/hangman/model/Game.java
+++ src/main/java/org/adani/hangman/model/Game.java
@@ -111,9 +111,10 @@ public class Game {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		int result = 7;
+		result = 31 * result + id.hashCode();
+		result = 31 * result + startTs.hashCode();
+		result = 31 * result + player.hashCode();
 		return result;
 	}
 
@@ -126,12 +127,10 @@ public class Game {
 		if (getClass() != obj.getClass())
 			return false;
 		Game other = (Game) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+		
+		return id.equals(other.id) &&
+				player.equals(other.player) &&
+				startTs.equals(other.startTs);
 	}
 
 	public static int getGuessAllowance() {
